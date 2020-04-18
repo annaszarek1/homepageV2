cd510b3ba37004e54a56b282897da49587d0f4c0
Anna Szarek <anna.szarek1@gmail.com>
2020-04-18 20:54:39 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const name = 'Ania';
+const age = 35;
+console.log(name);
+console.log(age);
+console.log(`Nazywam się ${name} i mam ${age} lat`);
+console.log('Witaj na mojej stronie');
+//alert(`Nazywam się ${name} i mam ${age} lat. 
+//Witaj na mojej stronie!`);
